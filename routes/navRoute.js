4cfde41aa8721f81a74e8fa9a58c722945490f8b
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-27 09:57:12 +0530

    share location and user position added

--- routes/navRoute.js
+++ routes/navRoute.js
@@ -3,23 +3,38 @@ const navigating = express.Router();
 const navigatingController = require('../controllers/navigatingController');
 
 let myLocation;
+let UserPositionID;
 navigating.post('/myLocation', async (req,res) => {
     const database = req.app.locals.database;
     myLocation = {
-        "latitude": latitude,
-        "longitude": longitude
+        "latitude": req.body.latitude,
+        "longitude": req.body.longitude
     };
-    const result = navigatingController.SetMyLocation(database, req.body.email, req.body.latitude, req.body.longitude);
-    if(result === true){
+    const result = await navigatingController.SetMyLocation(database, req.body.email, req.body.latitude, req.body.longitude);
+    const objectId = await navigatingController.SetUserLocation(database, req.body.latitude, req.body.longitude, UserPositionID);
+    if(objectId){
+        UserPositionID = objectId;
+    }
+    if(result){
+        UserPositionID = result
         res.status(200).send(true);
     }
     else{
         res.status(500).send(false);
     }
 });
+
 navigating.get('/getDangers', async (req,res) => {
     const database = req.app.locals.database;
-    const result = await navigatingController.GetDangers(database, myLocation)
+    const result = await navigatingController.GetDangers(database, myLocation);
 });
+
+navigating.get('/getUserLocation/:email', async (req,res) => {
+    const database = req.app.locals.database;
+    const email = req.params.email;
+    console.log(email);
+    const result = navigatingController.SharedUserLocation(database, email);
+    res.redirect(`https://www.google.com/maps?q=${result.latitude},${result.longitude}`);
+})
 
 module.exports = navigating;
