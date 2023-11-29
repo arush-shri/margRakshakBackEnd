af76bbb445dffcae00d3b29f91a46dee2ff502ae
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-29 22:10:44 +0530

    contri other area logic improvement

--- controllers/contributionController.js
+++ controllers/contributionController.js
@@ -1,9 +1,12 @@
-async function MakeContribution (database, collectionName, latitude, longitude) {
+async function MakeContribution (database, collectionName, latitude, longitude, name) {
     const check1 = await database.collection("AccidentArea").findOne({'location.coordinates': [ longitude, latitude ]});
     const check2 = await database.collection("RailwayCross").findOne({'location.coordinates': [ longitude, latitude ]});
     const check3 = await database.collection("ForestRoad").findOne({'location.coordinates': [ longitude, latitude ]});
     const check4 = await database.collection("GhatRegion").findOne({'location.coordinates': [ longitude, latitude ]});
     const check5 = await database.collection("OtherArea").findOne({'location.coordinates': [ longitude, latitude ]});
+    if(collectionName === "OtherArea"){
+        setOtherName(database, latitude, longitude, name);
+    }
     if(!(check1 && check2 && check3 && check4 && check5)){
         const collection = database.collection(collectionName);
         const query = {
@@ -24,4 +27,33 @@ async function MakeContribution (database, collectionName, latitude, longitude) 
     }
 }
 
-module.exports = { MakeContribution };
+async function setOtherName(database, latitude, longitude, name){
+    const collectionName = database.collection("OtherName");
+    const query = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "areaName": name
+    };
+    const result = await collectionName.insertOne(query);
+    if(result.acknowledged === true){
+        return true;
+    }
+    return false;
+}
+
+async function getOtherName(database, latitude, longitude){
+    const collectionName = database.collection("OtherName");
+    const query = {
+        "latitude": latitude,
+        "longitude": longitude,
+    };
+    const result = await collectionName.findOne(query);
+    if(result){
+        return result;
+    }
+    else{
+        return false;
+    }
+}
+
+module.exports = { MakeContribution, getOtherName };
--- routes/contributeRoute.js
+++ routes/contributeRoute.js
@@ -4,12 +4,24 @@ const contributionController = require('../controllers/contributionController')
 
 contributionRoute.post('/makeContribution', async (req,res) => {
     const database = req.app.locals.database;
-    const result = await contributionController.MakeContribution(database, req.body.collectionName, req.body.latitude, req.body.longitude);
+    const result = await contributionController.MakeContribution(database, req.body.collectionName, req.body.latitude,
+            req.body.longitude, req.body.name);
     if(result === true){
         res.status(200).send(true);
     }
     else{
         res.status(500).send(false);
+    }
+});
+
+contributionRoute.get('/getOtherName', async (req,res) => {
+    const database = req.app.locals.database;
+    const result = contributionController.getOtherName(database, req.body.latitude, req.body.longitude);
+    if(result){
+        res.status(200).send(result);
+    }
+    else{
+        res.status(404).send(false);
     }
 });
 
